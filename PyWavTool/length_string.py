9299334999cf0e676bfaef69943e2b5083b0c438
oatsu <oatsu.dev@gmail.com>
2025-08-30 12:56:41 +0900

    Apply formatter (ruff)

--- PyWavTool/length_string.py
+++ PyWavTool/length_string.py
@@ -1,7 +1,7 @@
-﻿def str2float(length :str) -> float:
-    '''
+﻿def str2float(length: str) -> float:
+    """
     UTAUのLength文字列をfloatに変換します。
-    
+
     Parameters
     ----------
     length :str
@@ -18,15 +18,15 @@
     | tick@tempo
     | tick@tempo+delta
     | tick@tempo-delta
-    
+
     | 戻り値の計算は以下の通りです。
     | 1拍あたりのms = 60*1000 / tempo
     | 1tickあたりのms = 1拍あたりのms / 480
     | length = 1tickあたりのms * tick +(-) delta
-    '''
+    """
     temp: str = length.split("@")
-    tempo :float
-    delta :float =0
+    tempo: float
+    delta: float = 0
     tick: int = int(temp[0])
     if "+" in temp[1]:
         tempo = float(temp[1].split("+")[0])
@@ -35,7 +35,6 @@
         tempo = float(temp[1].split("-")[0])
         delta = -float(temp[1].split("-")[1])
     else:
-        tempo=float(temp[1])
+        tempo = float(temp[1])
 
     return 60000 / tempo / 480 * tick + delta
-
